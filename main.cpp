1cd91111df07dad04a233ebad1732d62c34dd0c3
Daniel Jędrzejczyk <3.daniel.jedrzejczyk@gmail.com>
2018-12-19 14:14:49 +0100

    Aktualizacja scen

--- main.cpp
+++ main.cpp
@@ -17,7 +17,12 @@ using namespace std;
 
 int delta, lastTime;
 GLuint texture[1];
-int flaga_menu = 0;
+int flaga_new_game = 0;
+int flaga_options = 0;
+int flaga_info_game = 0;
+int flaga_exit = 0;
+int flag_fullscr = 0;
+int flaga_back_to_menu = 0;
 class Engine
 {
 private:
@@ -107,6 +112,7 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     main_menu(); // TEXTURA Z MENU GŁÓWNYM
+
     glutSwapBuffers();
 }
 
@@ -116,10 +122,13 @@ void reshape(int width, int height)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glViewport(0,0,width,height);
+    // Rzuty BEGIN
     gluPerspective(45,ratio,1,1000);
+    //glOrtho(0.0, (GLdouble) width, 0.0, (GLdouble) height, 100, -100);
+    // Rzuty END;
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(0.0,0.0,6.0,0.0,0.0,0.0,0.0f,1.0f,0.0f);
+   // gluLookAt(0.0,0.0,6.0,0.0,0.0,0.0,0.0f,1.0f,0.0f);
 }
 
 void klawiatura(unsigned char key, int x, int y);
@@ -127,12 +136,18 @@ void mouse(int button, int state, int x, int y);
 void onIdle();
 void new_game_texture();
 void info_game_texture();
+void main_menu_texture();
+void options_texture();
 int main(int argc, char *argv[])
 {
 
     Engine *game;
     game=game->getInstance();
-    flaga_menu = 1;
+    flaga_new_game = 1;
+    flaga_exit = 1;
+    flaga_info_game = 1;
+    flaga_options = 1;
+    flaga_back_to_menu = 1;
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
     glutInitWindowPosition(100,100);
@@ -151,7 +166,7 @@ int main(int argc, char *argv[])
     glEnable(GL_DEPTH_TEST);
     game->LoadGLTextures(); //<-- added this
     glDepthFunc(GL_LEQUAL);
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+   // glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     glutMainLoop();
 
     return 0;
@@ -167,10 +182,12 @@ void klawiatura(unsigned char key, int x, int y)
         break;
     case 49 :
         glutFullScreen();
+        flag_fullscr = 1;
         break;
     case 50 :
         glutReshapeWindow(800,600);
         glutInitWindowPosition(100,100);
+        flag_fullscr = 0;
         cout << "DRUGA OPCJA" << endl;
 
         break;
@@ -180,40 +197,112 @@ void klawiatura(unsigned char key, int x, int y)
 void mouse(int button, int state, int x, int y)
 {
     cout << x << "," << y << endl;
-    // FULL SCR :
-    // if(x >= 460 && x <= 872 && y >= 250 && y <= 296 && flaga_menu == 1)
-    // if(x >= 544 && x <= 761 && y >= 370 && y <= 416 && flaga_menu == 1)
-    // if(x >= 520 && x <= 790 && y >= 482 && y <= 531 && flaga_menu == 1)
-    // if(x >= 523 && x <= 781 && y >= 597 && y <= 648 && flaga_menu == 1)
-    // END
-    // Znalezc funkcje, ktora zwraca status okna!
-    if(x >= 271 && x <= 503 && y >= 194 && y <= 233 && flaga_menu == 1)
+    if(flag_fullscr == 1)
     {
-        cout << "NOWA GRA" << endl;
-        if(flaga_menu == 1)
+        // FULL SCR :
+        if(x >= 460 && x <= 872 && y >= 250 && y <= 296 && flaga_new_game == 1)
         {
-            flaga_menu = 0;
-            new_game_texture();
+            cout << "NOWA GRA" << endl;
+                flaga_new_game = 0;
+                flaga_info_game = 0;
+                flaga_options = 0;
+                flaga_exit = 0;
+                flaga_back_to_menu = 1;
+                new_game_texture();
         }
-    }
-    if(x >= 317 && x <= 450 && y >= 286 && y <= 325 && flaga_menu == 1)
-    {
-        cout << "OPCJE" << endl;
-    }
-    if(x >= 305 && x <= 463 && y >= 375 && y <= 413 && flaga_menu == 1)
-    {
-        cout << "O GRZE" << endl;
-        if(flaga_menu == 1)
+        if(x >= 544 && x <= 761 && y >= 370 && y <= 416 && flaga_options == 1)
         {
-            info_game_texture();
+            cout << "OPCJE" << endl;
+                flaga_new_game = 0;
+                flaga_info_game = 0;
+                flaga_options = 0;
+                flaga_exit = 0;
+                flaga_back_to_menu = 1;
+                options_texture();
         }
-    }
-    if(x >= 305 && x <= 460 && y >= 460 && y <= 506 && flaga_menu == 1)
-    {
-        cout << "Wyjdz" << endl;
-        exit(0);
-    }
+        if(x >= 520 && x <= 790 && y >= 482 && y <= 531 && flaga_info_game == 1)
+        {
+            cout << "O GRZE" << endl;
+            flaga_new_game = 0;
+            flaga_info_game = 0;
+            flaga_options = 0;
+            flaga_exit = 0;
+            flaga_back_to_menu = 1;
+                info_game_texture();
 
+        }
+        if(x >= 523 && x <= 781 && y >= 597 && y <= 648 && flaga_exit == 1)
+        {
+            cout << "Wyjdz" << endl;
+            exit(0);
+        }
+        if(x >= 523 && x <= 781 && y >= 597 && y <= 648 && flaga_back_to_menu == 1)
+        {
+
+                     cout << "Powrot" << endl;
+                     flaga_new_game = 1;
+                     flaga_info_game = 1;
+                     flaga_options = 1;
+
+                 flaga_back_to_menu = 0;
+                 main_menu_texture();
+        }
+
+    }
+    else
+    {
+        if(x >= 271 && x <= 503 && y >= 194 && y <= 233 && flaga_new_game == 1)
+        {
+            cout << "NOWA GRA" << endl;
+                flaga_new_game = 0;
+                flaga_info_game = 0;
+                flaga_options = 0;
+                flaga_exit = 0;
+                flaga_back_to_menu = 1;
+                new_game_texture();
+        }
+
+        if(x >= 317 && x <= 450 && y >= 286 && y <= 325 && flaga_options == 1)
+        {
+            cout << "OPCJE" << endl;
+                flaga_new_game = 0;
+                flaga_info_game = 0;
+                flaga_options = 0;
+                flaga_exit = 0;
+                flaga_back_to_menu = 1;
+                options_texture();
+        }
+        if(x >= 305 && x <= 463 && y >= 375 && y <= 413 && flaga_info_game == 1)
+        {
+            cout << "O GRZE" << endl;
+            flaga_new_game = 0;
+            flaga_info_game = 0;
+            flaga_options = 0;
+            flaga_exit = 0;
+            flaga_back_to_menu = 1;
+
+                info_game_texture();
+
+        }
+        if(x >= 305 && x <= 460 && y >= 460 && y <= 506 && flaga_exit == 1)
+        {
+
+                 cout << "Wyjdz" << endl;
+                 exit(0);
+        }
+        if(x >= 305 && x <= 460 && y >= 460 && y <= 506 && flaga_back_to_menu == 1)
+        {
+
+                    cout << "Powrot" << endl;
+                     flaga_new_game = 1;
+                     flaga_info_game = 1;
+                     flaga_options = 1;
+
+                 flaga_back_to_menu = 0;
+                 main_menu_texture();
+        }
+
+    }
 }
 
 void onIdle()
@@ -250,4 +339,30 @@ void info_game_texture()
                      SOIL_CREATE_NEW_ID,
                      SOIL_FLAG_INVERT_Y
                  );
+}
+//Texture in menu
+void main_menu_texture()
+{
+
+    glDeleteTextures(1,texture);
+    texture[0] = SOIL_load_OGL_texture
+                 (
+                     "menu.jpg",
+                     SOIL_LOAD_AUTO,
+                     SOIL_CREATE_NEW_ID,
+                     SOIL_FLAG_INVERT_Y
+                 );
+}
+//Texture in options
+void options_texture() {
+
+    glDeleteTextures(1,texture);
+    texture[0] = SOIL_load_OGL_texture
+                 (
+                     "opcje.jpg",
+                     SOIL_LOAD_AUTO,
+                     SOIL_CREATE_NEW_ID,
+                     SOIL_FLAG_INVERT_Y
+                 );
+
 }
